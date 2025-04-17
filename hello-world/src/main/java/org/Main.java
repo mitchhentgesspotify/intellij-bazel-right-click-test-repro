ddf4cd55017021e7ae90994b7b92df1c09fea757
Mitchell Hentges <mhentges@spotify.com>
2025-04-17 13:01:50 +0200

    Initial commit

---
+++ hello-world/src/main/java/org/Main.java
@@ -0,0 +1,12 @@
+package org;
+
+public class Main {
+
+  public static void main(String[] args) {
+    printString("Hello, World!");
+  }
+
+  public static void printString(String str) {
+    System.out.println(str);
+  }
+}
---
+++ hello-world/src/test/java/org/MainTest.java
@@ -0,0 +1,11 @@
+package org;
+
+import org.junit.Test;
+
+public class MainTest {
+
+  @Test
+  public void testPrintHelloWorld() {
+    System.out.println("Test success");
+  }
+}
